cf7a96c81a7e6bc960653fa8413b0d3de47805a4
sunflowerpoet <anna.simionato747@gmail.com>
2024-02-01 16:11:46 +0100

    first

--- src/main/java/com/example/esercizio1/Controller.java
+++ src/main/java/com/example/esercizio1/Controller.java
@@ -1,13 +1,13 @@
 package com.example.esercizio1;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
-	@GetMapping("/v1/ciao")
+	@RequestMapping ("/v1/ciao")
 	public String saluta(@RequestParam String nome, @RequestParam String provincia) {
 		return "Ciao " + nome + ", com'è il tempo in " + provincia + "?";
 	}
